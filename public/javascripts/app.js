152cf18db963d0826bdef96c9f014ada15445699
Coleman Kelleghan <coleman@kelleghandesign.com>
2016-05-12 14:35:27 -0600

    initial

---
+++ public/javascripts/app.js
@@ -0,0 +1,11 @@
+angular.module('redditClone', ['ngRoute'])
+
+// .config(function($routeProvider) {
+//
+//   $routeProvider
+//   .when('/', {
+//     templateUrl: 'views/reddit.html',
+//     controller: 'redditController'
+//   })
+//
+// })
---
+++ public/javascripts/controllers/redditController.js
@@ -0,0 +1,6 @@
+angular.module('redditClone')
+.controller('redditController', ['$scope', function($scope) {
+
+  $scope.testing = 123
+
+}])
